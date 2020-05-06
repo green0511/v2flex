e727ef819756ea65776bddf916a19d4a7c8cf3c4
green0511 <linhao_me@163.com>
2020-05-07 00:34:36 +0800

    feat: update card style

--- lib/containers/ItemList.dart
+++ lib/containers/ItemList.dart
@@ -39,172 +39,97 @@ class ItemsList extends StatelessWidget {
                   strokeWidth: 1,
                   child: Container(),
                 ),
-                // child: Container(
-                //   decoration: BoxDecoration(
-                //     // color: Colors.red,
-                //     border: Border.all(
-                //       width: 1,
-                //       color: Colors.black87,
-                //       style: BorderStyle.solid,
-                //     )
-                //   ),
-                // ),
               ),
               Container(
-                padding: EdgeInsets.only(top: 15, bottom: 10, left: 13, right: 13),
+                padding:
+                    EdgeInsets.only(top: 13, bottom: 15, left: 13, right: 13),
                 decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 240, 240, 240),
-                  border: Border.all(
-                    width: 1,
-                    color: Colors.black87,
-                  )
-                ),
+                    color: Color.fromARGB(255, 240, 240, 240),
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.black87,
+                    )),
                 child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Text(
+                      item.title,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 18,
+                        height: 1.3,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(right: 6),
+                            height: 16,
+                            padding: EdgeInsets.symmetric(horizontal: 5),
+                            decoration: BoxDecoration(
+                              color: Colors.black87,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(3)),
+                            ),
+                            child: Center(
+                              child: Text(
+                                item.node.name,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 9,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Text(
+                            item.author.name,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 14,
+                              // height: 1,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 5),
+                            child: Text(
+                              item.lastTouchString,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
                           Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: <Widget>[
-                                Text(
-                                  item.title,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    height: 1.3,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.only(top: 10),
-                                  child: Row(
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    children: <Widget>[
-                                      Container(
-                                        margin: EdgeInsets.only(right: 6),
-                                        height: 16,
-                                        padding: EdgeInsets.symmetric(
-                                          horizontal: 5
-                                        ),
-                                        decoration: BoxDecoration(
-                                          color: Colors.black87,
-                                          borderRadius: BorderRadius.all(Radius.circular(3)),
-                                        ),
-                                        child: Center(
-                                          child: Text(
-                                            item.node.name,
-                                            maxLines: 1,
-                                            overflow: TextOverflow.ellipsis,
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 9,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                      Text(
-                                        item.author.name,
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextStyle(
-                                          color: Colors.black54,
-                                          fontSize: 14,
-                                          // height: 1,
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.only(left: 5),
-                                        child: Text(
-                                          item.lastTouchString,
-                                          maxLines: 1,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: TextStyle(
-                                            color: Colors.black54,
-                                            fontSize: 12,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
+                            child: Text(''),
+                          ),
+                          Text(
+                            '${item.replyCount} 回复',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black54,
                             ),
                           ),
                         ],
                       ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        // widthFactor: 3,
-                        child: FractionallySizedBox(
-                          widthFactor: 0.3,
-                          child: Container(
-                            margin: EdgeInsets.only(top: 10),
-                            width: 100,
-                            height: 3,
-                            color: Colors.black12,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 10, bottom: 5),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            // IconTextButton('20 ups', Icons.arrow_upward),
-                            IconTextButton('${item.replyCount} 回复', Icons.chat_bubble),
-                            // IconTextButton('200 views', Icons.remove_red_eye),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
         );
       },
-    );
-  }
-
-}
-
-class IconTextButton extends StatelessWidget {
-  final String text;
-  final IconData iconData;
-
-  IconTextButton(this.text, this.iconData);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(right: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          // Container(
-          //   margin: EdgeInsets.only(right: 5),
-          //   child: Icon(
-          //     this.iconData,
-          //     size: 16,
-          //     color: Colors.black38,
-          //   ),
-          // ),
-          Text(
-            this.text,
-            style: TextStyle(
-              fontSize: 14,
-              height: 1,
-              color: Colors.black54,
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
